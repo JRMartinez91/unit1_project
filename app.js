ac91cdf28158e34ad46414ae4b39b6493fd1b4fb
James Martinez <airtorque1@gmail.com>
2020-03-31 20:58:12 -0400

    8x8 grid stored in array of arrays

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ $(()=>{
 
     //and heeeere weeee.......    go
 
+    //setup the 8x8 data structure
+    const grid = [ [],[],[],[],[],[],[],[]]
+
     //on page load, spawn 8x8 grid in game board
     //this will take the form of 8 row-divs which each contain 8 space-divs
     for(let i=0;i<8;i++){
@@ -16,10 +19,12 @@ $(()=>{
         for(let j=0; j<8; j++){
             $space = $('<div>').addClass("space")
             $row.append($space)
+            grid[i][j]= $space;
         }
         $gameboard.append($row)
     }
 
+    console.log(grid);
     //store the 8x8 grid in an array.
 
 
