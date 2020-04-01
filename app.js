6ca66088603d22c640473f933419ad7b7ed9e978
James Martinez <airtorque1@gmail.com>
2020-03-31 20:53:50 -0400

    gameboard has an 8x8 grid

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("Hello World")
+
+$(()=>{
+
+    //crucial jquery variables
+    $gameboard=$('#gameboard')
+    $whiteScore = $('#white-score')
+    $blackScore = $('#black-score')
+
+    //and heeeere weeee.......    go
+
+    //on page load, spawn 8x8 grid in game board
+    //this will take the form of 8 row-divs which each contain 8 space-divs
+    for(let i=0;i<8;i++){
+        $row = $('<div>').addClass("row");
+        for(let j=0; j<8; j++){
+            $space = $('<div>').addClass("space")
+            $row.append($space)
+        }
+        $gameboard.append($row)
+    }
+
+    //store the 8x8 grid in an array.
+
+
+
+})
